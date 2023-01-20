8e6370d3d81c0373481d150d83193423d675b26e
Rasmus Anker <info@rasmusanker.com>
2023-01-20 13:55:58 +0000

    added serial code

--- helpers/sendData.js
+++ helpers/sendData.js
@@ -1,35 +1,80 @@
-const assert = require('assert')
+// const assert = require('assert')
 
-const { getDevice } = require('./getDevice');
-const convertProntoCode = require('./convertProntoCode')
+// const { getDevice } = require('./getDevice');
+// const convertProntoCode = require('./convertProntoCode')
 
-module.exports = ({ host, hexData, log, name, logLevel }) => {
-  assert(hexData && typeof hexData === 'string', `\x1b[31m[ERROR]: \x1b[0m${name} sendData (HEX value is missing)`);
+// module.exports = ({ host, hexData, log, name, logLevel }) => {
+//   assert(hexData && typeof hexData === 'string', `\x1b[31m[ERROR]: \x1b[0m${name} sendData (HEX value is missing)`);
 
-  // Check for pronto code
-  if (hexData.substring(0, 4) === '0000') {
-    if (logLevel <= 1) {log(`\x1b[33m[DEBUG]\x1b[0m ${name} sendHex (Converting Pronto code "${hexData}" to Broadlink code)`);}
-    hexData = convertProntoCode(hexData, log);
-    if (logLevel <=1) {log(`\x1b[33m[DEBUG]\x1b[0m ${name} sendHex (Pronto code successfuly converted: "${hexData}")`);}
+//   // Check for pronto code
+//   if (hexData.substring(0, 4) === '0000') {
+//     if (logLevel <= 1) {log(`\x1b[33m[DEBUG]\x1b[0m ${name} sendHex (Converting Pronto code "${hexData}" to Broadlink code)`);}
+//     hexData = convertProntoCode(hexData, log);
+//     if (logLevel <=1) {log(`\x1b[33m[DEBUG]\x1b[0m ${name} sendHex (Pronto code successfuly converted: "${hexData}")`);}
     
-    if (!hexData) {return log(`\x1b[31m[ERROR] \x1b[0m${name} sendData (A Pronto code was detected however its conversion to a Broadlink code failed.)`);}
+//     if (!hexData) {return log(`\x1b[31m[ERROR] \x1b[0m${name} sendData (A Pronto code was detected however its conversion to a Broadlink code failed.)`);}
 
-  }
+//   }
 
-  // Get the Broadlink device
-  const device = getDevice({ host, log });
+//   // Get the Broadlink device
+//   const device = getDevice({ host, log });
 
-  if (!device) {
-    if (!host) {return log(`\x1b[31m[ERROR] \x1b[0m${name} sendData (no device found, ensure the device is not locked)`);}
+//   if (!device) {
+//     if (!host) {return log(`\x1b[31m[ERROR] \x1b[0m${name} sendData (no device found, ensure the device is not locked)`);}
 
-    return log(`\x1b[31m[ERROR] \x1b[0m${name} sendData (no device found at ${host}, ensure the device is not locked)`);
-  }
+//     return log(`\x1b[31m[ERROR] \x1b[0m${name} sendData (no device found at ${host}, ensure the device is not locked)`);
+//   }
 
-  if (!device.sendData) {return log(`\x1b[31m[ERROR] \x1b[0mThe device at ${device.host.address} (${device.host.macAddress}) doesn't support the sending of IR or RF codes.`);}
-  if (hexData.includes('5aa5aa555')) {return log(`\x1b[31m[ERROR] \x1b[0mThis type of hex code (5aa5aa555...) is no longer valid. Use the included "Learn Code" accessory to find new (decrypted) codes.`);}
+//   if (!device.sendData) {return log(`\x1b[31m[ERROR] \x1b[0mThe device at ${device.host.address} (${device.host.macAddress}) doesn't support the sending of IR or RF codes.`);}
+//   if (hexData.includes('5aa5aa555')) {return log(`\x1b[31m[ERROR] \x1b[0mThis type of hex code (5aa5aa555...) is no longer valid. Use the included "Learn Code" accessory to find new (decrypted) codes.`);}
 
-  const hexDataBuffer = new Buffer(hexData, 'hex');
-  device.sendData(hexDataBuffer, logLevel, hexData);
+//   const hexDataBuffer = new Buffer(hexData, 'hex');
+//   device.sendData(hexDataBuffer, logLevel, hexData);
 
-  if (logLevel <=2) {log(`${name} sendHex (${device.host.address}; ${device.host.macAddress}) ${hexData}`);}
+//   if (logLevel <=2) {log(`${name} sendHex (${device.host.address}; ${device.host.macAddress}) ${hexData}`);}
+// }
+// const assert = require('assert')
+
+// const { getDevice } = require('./getDevice');
+// const convertProntoCode = require('./convertProntoCode')
+const { SerialPort } = require('serialport');
+const port = new SerialPort({
+  path: '/dev/ttyACM0',
+  baudRate: 115200,
+})
+module.exports = ({ host, hexData, log, name, debug }) => {
+  // assert(hexData && typeof hexData === 'string', `\x1b[31m[ERROR]: \x1b[0m${name} sendData (HEX value is missing)`);
+
+  // // Check for pronto code
+  // if (hexData.substring(0, 4) === '0000') {
+  //   if (debug) log(`\x1b[33m[DEBUG]\x1b[0m ${name} sendHex (Converting Pronto code "${hexData}" to Broadlink code)`);
+  //   hexData = convertProntoCode(hexData, log)
+  //   if (debug) log(`\x1b[33m[DEBUG]\x1b[0m ${name} sendHex (Pronto code successfuly converted: "${hexData}")`);
+    
+  //   if (!hexData) return log(`\x1b[31m[ERROR] \x1b[0m${name} sendData (A Pronto code was detected however its conversion to a Broadlink code failed.)`);
+
+  // }
+
+  // // Get the Broadlink device
+  // const device = getDevice({ host, log });
+
+  // if (!device) {
+  //   if (!host) return log(`\x1b[31m[ERROR] \x1b[0m${name} sendData (no device found)`);
+
+  //   return log(`\x1b[31m[ERROR] \x1b[0m${name} sendData (no device found at ${host})`);
+  // }
+
+  // if (!device.sendData) return log(`\x1b[31m[ERROR] \x1b[0mThe device at ${device.host.address} (${device.host.macAddress}) doesn't support the sending of IR or RF codes.`);
+  // if (hexData.includes('5aa5aa555')) return log(`\x1b[31m[ERROR] \x1b[0mThis type of hex code (5aa5aa555...) is no longer valid. Use the included "Learn Code" accessory to find new (decrypted) codes.`);
+
+  // const hexDataBuffer = new Buffer(hexData, 'hex');
+  // device.sendData(hexDataBuffer, debug, hexData);
+  port.write(hexData, function(err) {
+    if (err) {
+      return log('Error on write: ', err.message)
+    }
+    
+  })
+  return log('message written');
+  // log(`${name} sendHex (${device.host.address}; ${device.host.macAddress}) ${hexData}`);
 }
